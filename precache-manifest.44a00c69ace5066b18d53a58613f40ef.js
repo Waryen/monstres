41b84748719b73ffe07fa575b655ab6f76ba664f
Jonathan Gomand <jonathangomand@gmail.com>
2020-10-19 15:00:27 +0200

    Updates

---
+++ precache-manifest.44a00c69ace5066b18d53a58613f40ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe59f281bae06bb3cf4279ef74bd1028",
+    "url": "/monstres/index.html"
+  },
+  {
+    "revision": "e3d894409b8d4c7eaf05",
+    "url": "/monstres/static/css/main.21c8f50d.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monstres/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstres/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3d894409b8d4c7eaf05",
+    "url": "/monstres/static/js/main.4a0de1fa.chunk.js"
+  },
+  {
+    "revision": "a9551688f4ae865174fb",
+    "url": "/monstres/static/js/runtime-main.90cbb0c4.js"
+  }
+]);
